94691b120088f970871a594386b427b3eb50c936
Alex <alexb@student.chalmers.se>
2021-12-02 15:40:24 +0100

    init

---
+++ day1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <thread>
+
+int part1(const std::vector<int>& input) {
+
+    int countLarger = 0;
+
+    for (int i = 1; i < input.size(); i++) {
+        if (input.at(i) > input.at(i-1)) {
+            countLarger++;
+        }
+    }
+
+    return countLarger;
+}
+
+int part2(const std::vector<int>& input) {
+
+    int previousSum = -1;
+    int currentSum;
+    int countLarger = 0;
+    
+    for (int i = 0; i < input.size() - 2; i++) {
+        currentSum = input.at(i) + input.at(i+1) + input.at(i+2);
+        if (currentSum > previousSum && previousSum != -1) {
+            countLarger++;
+        }
+        previousSum = currentSum;
+    }
+
+    return countLarger;
+}
+
+int main() {
+
+    std::ifstream f("day1_input.txt");
+
+    int value;
+    std::vector<int> input;
+    while (f >> value) input.push_back(value);
+
+    std::cout << "Part 1: " << part1(input) << std::endl;
+    std::cout << "Part 2: " << part2(input) << std::endl;
+
+    return 0;
+}
---
+++ day2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+int part1(vector<tuple<string, int>>& input) {
+    
+    int h = 0;
+    int d = 0;
+
+    for (auto [direction, value] : input) {
+       
+        if (direction == "forward") {
+            h += value;
+        } else if (direction == "up") {
+            d -= value;
+        } else if (direction == "down") {
+            d += value;
+        }
+    }
+
+    return h * d;
+}
+
+int part2(vector<tuple<string, int>>& input) {
+
+    int h = 0;
+    int d = 0;
+    int aim = 0;
+
+    for (auto [direction, value] : input) {
+
+        if (direction == "forward") {
+            h += value;
+            d += aim * value;
+        } else if (direction == "up") {
+            aim -= value;
+        } else if (direction == "down") {
+            aim += value;
+        }
+    }
+
+    return h * d;
+}
+
+int main() {
+    
+    fstream f("day2_input.txt");
+    
+    string direction;
+    int value;
+    vector<tuple<string, int>> input;
+    while (f >> direction >> value) {
+        input.emplace_back(direction, value);
+    }
+
+    std::cout << "Part1: " << part1(input) << std::endl;
+    std::cout << "Part2: " << part2(input) << std::endl;
+    
+    return 0;
+}
