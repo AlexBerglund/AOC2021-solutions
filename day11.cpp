cd55985cc70849815b197bb67de175bc51fd43dd
Alex <alexb@student.chalmers.se>
2021-12-11 19:48:07 +0100

    update day 11

--- day11.cpp
+++ day11.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <array>
-#include <vector>
-#include <set>
 
 using namespace std;
 
@@ -10,97 +8,99 @@ bool invalid(int y, int x) {
     return (x < 0 || x > 9 || y < 0 || y > 9);
 }
 
-void flash(int y, int x, array<array<int, 10>, 10>& grid, const set<int>& flashed) {
+void flash(int y, int x, array<array<int, 10>, 10>& grid, bool first_iter) {
     grid[y][x] = 0;
-    for (int i = y-1; i <= y+1; i++) {
-        for (int j = x-1; j <= x+1; j++) {
-            bool already_flashed = flashed.find(i*10 + j) != flashed.end();
-            if (i == y && j == x || invalid(i, j) || already_flashed) continue;
+    for (int i = y - 1; i <= y + 1; i++) {
+        for (int j = x - 1; j <= x + 1; j++) {
+            if (i == y && j == x || invalid(i, j)) continue;
+
+            if (!first_iter && grid[i][j] == 0) continue;
+
+            if (grid[i][j] == 0 && i*10 + j < y*10 + x) continue;
+
             grid[i][j]++;
         }
     }
 }
 
-int update_tick(array<array<int, 10>, 10>& grid, set<int>& flashed) {
-    int flash_count = 0;
+int update_tick(array<array<int, 10>, 10>& grid) {
+    int count = 0;
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             grid[i][j]++;
             if (grid[i][j] > 9) {
-                flash(i, j, grid, flashed);
-                flashed.insert(i*10 + j);
-                flash_count++;
+                flash(i, j, grid, true);  
+                count++;  
             }
         }
     }
-    return flash_count;
+    return count;
 } 
 
-int update_aftermath(array<array<int, 10>, 10>& grid, set<int>& flashed) {
-    int flash_count = 0;
+int update_aftermath(array<array<int, 10>, 10>& grid) {
+    int count = 0;
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             if (grid[i][j] > 9) {
-                flash(i, j, grid, flashed);
-                flashed.insert(i*10 + j);
-                flash_count++;
+                flash(i, j, grid, false);
+                count++;
             }
         }
     }
-    return flash_count;
+    return count;
 }
 
-int part1(array<array<int, 10>, 10> grid) {
-    set<int> flashed;
-    int total_flashes = 0;
-    int flash_count;
-    
-    for (int i = 0; i < 100; i++) {
-    
-        total_flashes += update_tick(grid, flashed);
-        do {
-            flash_count = update_aftermath(grid, flashed);
-            total_flashes += flash_count;
-        } while (flash_count);
-
-        flashed.clear();
+bool all_zero(const array<array<int, 10>, 10>& grid) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (grid[i][j] != 0) return false;
+        }
     }
-    
-    return total_flashes;
+    return true;
 }
 
-int part2(array<array<int, 10>, 10>& grid) {
-    set<int> flashed;
-    bool any_flashed;
-
-    int step = 0;
-    while (true) {
-        step++;
-
-        update_tick(grid, flashed);
+int part1(array<array<int, 10>, 10> grid, int steps) {
+    int total_count = 0;
+    int count;
+    while (steps > 0) {
+        total_count += update_tick(grid);
         do {
-            any_flashed = update_aftermath(grid, flashed);
+            count = update_aftermath(grid);
+            total_count += count;
+        } while (count);
+        steps--;
+    }
+    return total_count;
+}
+
+int part2(array<array<int, 10>, 10> grid) {
+    int steps = 0;
+    bool any_flashed;
+    while (true) {
+        steps++;
+        update_tick(grid);
+        do {
+            any_flashed = update_aftermath(grid);
         } while (any_flashed);
-
-        if (flashed.size() == 100) return step;
-
-        flashed.clear();
+        if (all_zero(grid)) return steps;
     }
 }
 
 int main() {
 
-    array<array<int, 10>, 10> input;
-    ifstream f("day11_input.txt");
+    array<array<int, 10>, 10> grid;
+    ifstream f("input/day11_input.txt");
 
     char num;
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             f >> num;
-            input[i][j] = num - '0';
+            grid[i][j] = num - '0';
         }
     }
 
-    cout << "Part 1: " << part1(input) << endl;
-    cout << "Part 2: " << part2(input) << endl;
+    cout << "Part 1: " << part1(grid, 100) << endl;
+    cout << "Part 2: " << part2(grid) << endl;
+
+    return 0;
 }
